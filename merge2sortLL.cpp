d39b01ca35ed2b0cd05de89c4f891a29826feed3
aryan9855 <aryansinghalagra@gmail.com>
2025-05-19 12:26:15 +0530

    LC QUES: 21

---
+++ merge2sortLL.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/
+// 21. Merge Two Sorted Lists
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL){
+            return list2;
+        }
+        
+        if(list2 == NULL){
+            return list1;
+        }
+
+        if(list1 -> val < list2 -> val){
+            list1 -> next = mergeTwoLists(list1-> next , list2);
+            return list1;
+        }
+           list2 -> next = mergeTwoLists(list2-> next , list1);
+            return list2;
+
+    }
+};
