ac936fc8a2808045a720c1785d2cacd37e12e4ca
aryan9855 <aryansinghalagra@gmail.com>
2025-05-20 12:13:37 +0530

    LC ques 2

---
+++ add2numbers.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/add-two-numbers/
+// Leetcode problem 2
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* curr= dummy ;
+        int carry =0;
+        while(l1 != NULL || l2 != NULL || carry !=0){
+            int sum =0;
+            if(l1 != NULL){
+                sum += l1->val;
+                l1 = l1-> next;
+            }
+             if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2-> next;
+            }
+            sum +=carry;
+            carry = sum /10;
+            int digit = sum %10;
+            ListNode*  newNode = new ListNode(digit);
+            curr -> next = newNode;
+            curr = curr -> next;
+        }
+        
+        return dummy -> next;
+    }
+};
