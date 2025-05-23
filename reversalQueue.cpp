54e6b7bccd9f6c9b128cfd9b68ef2877ad48e47b
aryan9855 <aryansinghalagra@gmail.com>
2025-05-23 11:18:28 +0530

    GFG

---
+++ reversalQueue.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    queue<int> reverseQueue(queue<int> &q) {
+        // code here.
+        stack<int> s;
+        while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+        }   
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+        return q;
+    }
+};
